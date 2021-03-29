e6c4c7f4056006955480e7cf7dac7ed6187f4011
qixiaoling <x.qi@novi-education.nl>
2021-03-29 11:24:54 +0200

    login page css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react'
 import './Login.css'
+
 export default class Login extends Component{
+
     render() {
         return (
-            <div>
-                <h2>You are about to sign in</h2>
+            <div className="login-popup">
+                <div className="box">
+                    <div className="img-area">
+                        <div className="img">
+                        </div>
+                        <h1 className="img-title">eGineRight</h1>
+                    </div>
+                    <div className="form">
+                        <div className="close">&times;</div>
+                        <h1 className="login-popup-title">Sign In</h1>
+                        <form>
+                            <div className="form-group">
+                                <input type="text" placeholder="username" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" placeholder="password" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <input type="checkbox"/>
+                                <label>   Remember Me
+                                </label>
+                            </div>
+                            <button type="button" className="btn">Sign In</button>
+                        </form>
+                    </div>
+
+                </div>
             </div>
         );
     }
