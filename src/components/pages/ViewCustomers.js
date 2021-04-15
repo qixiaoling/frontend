cad2ce38eddaf56f9ca842b69ee4d1b0a1b66f4f
qixiaoling <x.qi@novi-education.nl>
2021-04-15 09:22:16 +0200

    ViewCustomer.css exceeding border need to fix

--- src/components/pages/ViewCustomers.js
+++ src/components/pages/ViewCustomers.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import CustomerService from "../../services/CustomerService";
+import './ViewCustomer.css'
 
 class ViewCustomers extends Component {
     constructor(props) {
@@ -16,24 +17,88 @@ class ViewCustomers extends Component {
     }
     render(){
         return(
-            <div>
-                <div>
-                    <h3>View Customer Details</h3>
-                    <div>
-                        <div>
-                            <label> Customer First Name: </label>
+            <div className="main-container-view-customer">
+                <div className="information-container-view-customer">
+                    <h2>View Customer Details</h2>
+                    <div className="view-customer-card-body">
+                        <div className="view-customer-element">
+                            <label> First Name: </label>
                             <div>{this.state.customer.firstName}</div>
                         </div>
-                        <div>
-                            <label> Customer Last Name: </label>
+                        <div className="view-customer-element">
+                            <label> Last Name: </label>
                             <div>{this.state.customer.lastName}</div>
                         </div>
-                        <div>
-                            <label> Customer Gender: </label>
+                        <div className="view-customer-element">
+                            <label> Gender: </label>
                             <div>{this.state.customer.gender}</div>
                         </div>
-                        <div>
-                            <label> Customer Email: </label>
+                        <div className="view-customer-element">
+                            <label> Email: </label>
+                            <div>{this.state.customer.email}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> First Name: </label>
+                            <div>{this.state.customer.firstName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Last Name: </label>
+                            <div>{this.state.customer.lastName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Gender: </label>
+                            <div>{this.state.customer.gender}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Email: </label>
+                            <div>{this.state.customer.email}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> First Name: </label>
+                            <div>{this.state.customer.firstName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Last Name: </label>
+                            <div>{this.state.customer.lastName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Gender: </label>
+                            <div>{this.state.customer.gender}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Email: </label>
+                            <div>{this.state.customer.email}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> First Name: </label>
+                            <div>{this.state.customer.firstName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Last Name: </label>
+                            <div>{this.state.customer.lastName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Gender: </label>
+                            <div>{this.state.customer.gender}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Email: </label>
+                            <div>{this.state.customer.email}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> First Name: </label>
+                            <div>{this.state.customer.firstName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Last Name: </label>
+                            <div>{this.state.customer.lastName}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Gender: </label>
+                            <div>{this.state.customer.gender}</div>
+                        </div>
+                        <div className="view-customer-element">
+                            <label> Email: </label>
                             <div>{this.state.customer.email}</div>
                         </div>
                     </div>
