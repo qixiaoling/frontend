045a0303f3d12cfd3a77f2b789ffe255106622e4
qixiaoling <x.qi@novi-education.nl>
2021-04-28 09:01:05 +0200

    correction deleted token section in js

--- src/components/InspectionPages/ListInspections.js
+++ src/components/InspectionPages/ListInspections.js
@@ -1,21 +1,6 @@
 import React, {Component} from 'react'
 import InspectionService from "../../services/InspectionService";
 import'../CusotmerPages/ListCustomers.css'
-import axios from "axios";
-
-
-const accessToken =
-    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJPbGFmIiwiYXV0aG9yaXRpZXMiOlt7ImF1dGhvcml0eSI6IkFETUlOIn0seyJhdXRob3JpdHkiOiJVU0VSX1RSRSJ9LHsiYXV0aG9yaXR5IjoiVVNFUl9GUk8ifSx7ImF1dGhvcml0eSI6IlVTRVJfVEVDIn0seyJhdXRob3JpdHkiOiJVU0VSX0JBQyJ9XSwiaWF0IjoxNjE4MzkxNjIwLCJleHAiOjE2MTk1NjA4MDB9.dtSmT_MCQw8Z2OtVv8f_Yag8b_vsTflMkZdZ3M_NrcgtjtedGJaS77nFWTMcCx5ZvzB1n9AnNpcixOCDOIsYew'
-const apiUrl = 'http://localhost:8080/customers';
-axios.interceptors.request.use(
-    config=> {
-        config.headers.authorization = `Bearer ${accessToken}`;
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    }
-)
 
 class ListInspections extends Component{
     constructor(props) {
--- src/components/InventoryPages/UpdateInventories.js
+++ src/components/InventoryPages/UpdateInventories.js
@@ -1,20 +1,6 @@
 import React, {Component} from 'react'
-import axios from "axios";
 import '../CusotmerPages/CreateCustomers'
 import InventoryService from "../../services/InventoryService";
-
-const accessToken =
-    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJPbGFmIiwiYXV0aG9yaXRpZXMiOlt7ImF1dGhvcml0eSI6IkFETUlOIn0seyJhdXRob3JpdHkiOiJVU0VSX1RSRSJ9LHsiYXV0aG9yaXR5IjoiVVNFUl9GUk8ifSx7ImF1dGhvcml0eSI6IlVTRVJfVEVDIn0seyJhdXRob3JpdHkiOiJVU0VSX0JBQyJ9XSwiaWF0IjoxNjE4MzkxNjIwLCJleHAiOjE2MTk1NjA4MDB9.dtSmT_MCQw8Z2OtVv8f_Yag8b_vsTflMkZdZ3M_NrcgtjtedGJaS77nFWTMcCx5ZvzB1n9AnNpcixOCDOIsYew'
-const apiUrl = 'http://localhost:8080/customers';
-axios.interceptors.request.use(
-    config=> {
-        config.headers.authorization = `Bearer ${accessToken}`;
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    }
-)
 
 class UpdateInventories extends Component{
     constructor(props) {
