33f54df21fba8cd350e8a36d9e9c3c512e94b1e7
qixiaoling <x.qi@novi-education.nl>
2021-05-07 09:33:55 +0200

    login, changed interceptors.use providing localStorage.getItem('token') instead of accessToken (string!)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,75 @@
 import React, {Component} from 'react'
 import './Login.css'
 import axios from "axios";
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Redirect } from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 
-class Login extends Component{
+class Login extends Component {
 
     constructor() {
         super();
 
-        this.state = {
-
-        }
+        this.state = {}
     }
 
-    handleSubmit = e =>{
+    handleSubmit = e => {
         e.preventDefault();
 
-    const data = {
-        username: this.username,
-        password: this.password
-    }
-    console.log(data)
-    axios.post('http://localhost:8080/login', data)
-        .then(res =>{
+        const data = {
+            username: this.username,
+            password: this.password
+        }
+        console.log(data)
+        axios.post('http://localhost:8080/login', data)
+            .then(res => {
 
-            localStorage.setItem('token', res.headers.authorization);
-            this.setState({
-                loggedIn: true
+                localStorage.setItem('token', res.headers.authorization);
+                this.setState({
+                    loggedIn: true
+                })
+                console.log(res.headers.authorization)
             })
-            console.log('Look at me! I am Login')
-        })
 
 
-}
+    }
 
 
-    render(){
-        if(this.state.loggedIn){
-            return <Redirect to={'/'} />
+
+    render() {
+        if (this.state.loggedIn) {
+            return <Redirect to={'/'}/>
         }
         return (
 
-                <div className="login-popup">
-                    <div className="box">
-                        <div className="img-area">
-                            <div className="img">
-                            </div>
-                            <h1 className="img-title">eGineRight</h1>
+            <div className="login-popup">
+                <div className="box">
+                    <div className="img-area">
+                        <div className="img">
                         </div>
-                        <div className="form">
-                            <div className="close">&times;</div>
-                            <h1 className="login-popup-title">Sign In</h1>
-                            <form>
-                                <div className="form-group">
-                                    <input type="text" placeholder="username" className="form-control"
-                                    onChange={e => this.username= e.target.value}/>
-                                </div>
-                                <div className="form-group">
-                                    <input type="password" placeholder="password" className="form-control"
-                                           onChange={e => this.password= e.target.value}/>
-                                </div>
-                                <div className="form-group">
-                                    <input type="checkbox"/>
-                                    <label>   Remember Me
-                                    </label>
-                                </div>
-                                <button type="button" className="btn" onClick={this.handleSubmit}>Sign In</button>
-                            </form>
-                        </div>
-
+                        <h1 className="img-title">eGineRight</h1>
                     </div>
+                    <div className="form">
+                        <div className="close">&times;</div>
+                        <h1 className="login-popup-title">Sign In</h1>
+                        <form>
+                            <div className="form-group">
+                                <input type="text" placeholder="username" className="form-control"
+                                       onChange={e => this.username = e.target.value}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" placeholder="password" className="form-control"
+                                       onChange={e => this.password = e.target.value}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="checkbox"/>
+                                <label> Remember Me
+                                </label>
+                            </div>
+                            <button type="button" className="btn" onClick={this.handleSubmit}>Sign In</button>
+                        </form>
+                    </div>
+
                 </div>
+            </div>
 
 
         );
@@ -80,5 +77,6 @@ class Login extends Component{
     }
 
 }
+
 export default Login
 
